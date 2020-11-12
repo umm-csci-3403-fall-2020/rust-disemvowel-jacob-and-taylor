f0fed48c325f9a67e4001f0399e57d06ecddc70a
carri320 <carri320@morris.umn.edu>
2020-11-12 10:52:23 -0600

    Got the second TODO working

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,14 @@ fn main() {
     //  * Write the disemvoweled text using write_file
 
     // Replace String::from("test") with what you get from read_file
-    let s = String::from("dummy text");
+    let filePath = Path::new(&arg[1]);
+    let s = read_file(filePath);
 
     let s_disemvowel = disemvowel(&s);
 
     // Use command-line arguments for the name of the file,
     // and s_disemvowel for the text to write out.
-    write_file(Path::new("dummy.txt"), "output string");
+    write_file(Path::new(&args[2]), s_disemvowel);
 }
 
 fn read_file(path: &Path) -> String {
